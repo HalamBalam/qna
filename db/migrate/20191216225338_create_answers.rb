8ecca5d6aae356d87a0bdb36a9a60681b6c1c3d2
Anton Surganov <antonsurganov@yandex.ru>
2019-12-18 02:07:54 +0300

    Corrections

--- db/migrate/20191216225338_create_answers.rb
+++ db/migrate/20191216225338_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.integer :question_id
-      t.string :body
+      t.integer :question_id, null: false
+      t.string :body, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,15 @@ ActiveRecord::Schema.define(version: 2019_12_16_225338) do
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.integer "question_id"
-    t.string "body"
+    t.integer "question_id", null: false
+    t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
